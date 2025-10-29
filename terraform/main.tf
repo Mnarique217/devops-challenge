13d294ea7ebf4e75d6ef06199a208c488c11072d
Manrique Arrieta <manri.arf15@gmail.com>
2025-10-29 09:48:45 -0600

    feat(infra): added backend to terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,19 @@
+# https://developer.hashicorp.com/terraform/language/backend/s3
+terraform {
+  backend "s3" {
+    bucket = "terraform-tfstate"
+    key    = "kubernetes/minikube"
+    region = "us-east-1"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
+  }
+}
+
+# https://registry.terraform.io/providers/scott-the-programmer/minikube/latest/docs/resources/cluster#insecure_registry-1
 resource "minikube_cluster" "docker" {
-  driver       = var.driver
-  cluster_name = var.cluster_name
-  addons = var.addons
+  driver            = var.driver
+  cluster_name      = var.cluster_name
+  addons            = var.addons
   insecure_registry = var.insecure_registry
 }
